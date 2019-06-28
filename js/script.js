874117cf57a1375513d5218c56f1ecb4239daa3a
Anastasiya Turenko <oxtnpi@gmail.com>
2019-06-29 01:32:55 +0300

    Добавляет JavaScript и анимацию

---
+++ js/script.js
@@ -0,0 +1,101 @@
+var link = document.querySelector(".feedback-link");
+var feedbackPopup = document.querySelector(".feedback-modal");
+var modals = document.querySelectorAll(".modal");
+var mapLink = document.querySelector(".map-link");
+var mapPopup = document.querySelector(".modal-map");
+var feedbackForm = document.querySelector(".feedback-form");
+var feedbackName = document.querySelector(".feedback-name");
+var feedbackEmail = document.querySelector(".feedback-email");
+var feedbackMessage = document.querySelector(".feedback-message");
+var emailStorage = localStorage.getItem("feedbackEmail");
+var nameStorage = localStorage.getItem("feedbackName");
+var isStorageSupport = true;
+var storage = "";
+
+try {
+    storage = localStorage.getItem("feedbackName");
+} catch (err) {
+    isStorageSupport = false;
+}
+
+link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+
+    modals.forEach(function (item) {
+        if (item.classList.contains("modal-show")) {
+            item.classList.remove("modal-show");
+        }
+    });
+
+    feedbackPopup.classList.add("modal-show");
+    feedbackName.focus();
+
+    if (nameStorage) {
+        feedbackName.value = nameStorage;
+    }
+    if (emailStorage) {
+        feedbackEmail.value = emailStorage;
+    }
+
+    if (storage) {
+        feedbackName.value = storage;
+        feedbackEmail.value = storage;
+        feedbackMessage.focus();
+    } else {
+        feedbackName.focus();
+    }
+});
+
+
+mapLink.addEventListener("click", function (evt) {
+    evt.preventDefault();
+
+    modals.forEach(function (item) {
+
+        if (item.classList.contains("modal-show")) {
+            item.classList.remove("modal-show");
+        }
+    });
+
+    mapPopup.classList.add("modal-show");
+});
+
+modals.forEach(function (item) {
+    item.addEventListener("click", function (evt) {
+
+        if (evt.target.classList.contains("modal-close")) {
+            evt.preventDefault();
+            item.classList.remove("modal-show");
+            feedbackPopup.classList.remove("modal-error");
+        }
+    });
+});
+
+feedbackForm.addEventListener("submit", function (evt) {
+
+    if (!feedbackName.value ||
+        !feedbackEmail.value || !feedbackMessage.value) {
+        evt.preventDefault();
+        feedbackPopup.classList.remove("modal-error");
+        feedbackPopup.offsetWidth = feedbackPopup.offsetWidth;
+        feedbackPopup.classList.add("modal-error");
+    } else {
+        if (isStorageSupport) {
+            localStorage.setItem("feedbackName", feedbackName.value);
+            localStorage.setItem("feedbackEmail", feedbackEmail.value);
+        }
+    }
+});
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+        evt.preventDefault();
+        modals.forEach(function (item) {
+
+            if (item.classList.contains("modal-show")) {
+                item.classList.remove("modal-show");
+                feedbackPopup.classList.remove("modal-error");
+            }
+        });
+    }
+});
